7881fcf2fd5fc0a9553980b58b99b84822d4ef5d
Rajan Sahani <70761969+rjsany799@users.noreply.github.com>
2024-03-20 15:49:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Loginsignup from './components/login-signup/Loginsignup'; 
+
+function App() {
+  return (
+    <div>
+      <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
